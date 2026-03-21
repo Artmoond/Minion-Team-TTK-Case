59cf8acd3e979a736aedd9d873888a7c5101e5d1
Artmoond <artem.shapoval.00@inbox.ru>
2026-03-21 18:26:43 +0300

    IT'S ALIVE, IT'S ALIVE!

--- Backend/main_page/src/main/java/github/artmoond/Main.java
+++ Backend/main_page/src/main/java/github/artmoond/Main.java
@@ -3,9 +3,15 @@ package github.artmoond;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -20,11 +26,10 @@ public class Main {
      String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-    /*
-    @GetMapping("/player")
-    String player(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @GetMapping(value = "stream/StartAudio", produces= "audio/mpeg")
+    public ResponseEntity<InputStreamResource> StartAudio() throws IOException {
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/static/audio/NITSHEMERTW.mp3");
+        InputStreamResource resource = new InputStreamResource((new FileInputStream(file)));
+        return ResponseEntity.ok().contentLength(file.length()).body(resource);
     }
-
-     */
 }
